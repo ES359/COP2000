16b2a5f21a7e6dae2a5d6b2d5a5eb1b5e18949cf
ES359 <sphinxofpsl@gmail.com>
2016-01-14 16:17:25 -0500

    Initial commit

---
+++ src/ES/COP2000/Main.java
@@ -0,0 +1,43 @@
+package ES.COP2000;
+
+/**
+ * Created by ES359 on 1/14/16.
+ */
+public class Main
+{
+    public static void main(String args[])
+    {
+
+        double dPS = 20;
+        double rad = dPS/2;
+        double pi = 3.14;
+        double area  = pi * (rad)*2;
+
+        System.out.println("You can get "+area/10 + " pizza slices out of this large pizza.");
+        System.out.println();
+    }
+}
+/**
+ *
+ * For the output you want how many pizza's can you get in a large pie.
+ *
+ *
+ *
+ *
+ *
+ * The input is going to take an radius.
+ *
+ * Variables that are needed are 1 input variable(s).
+ *
+ * Var 1, diameter.
+ *
+ * Once you get the correct values, they need to be calculated correctly.
+ *
+ * Pie = 3.14
+ *
+ * You will need to calculate the size.
+ * radius =DiameterPieSize /2
+ *
+ * calculation  = * Pie * (radius *radius)
+ *
+ */
